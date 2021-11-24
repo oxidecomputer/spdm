593a1c8a1e2a8c7f7c8089ae313bafcb2718c84d
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2021-11-24 16:38:22 -0500

    Add License info and headers

--- src/msgs/mod.rs
+++ src/msgs/mod.rs
@@ -1,3 +1,9 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
 //! The `msgs` module contains all messages defined by the SPDM specification.
 //!
 //! Messages are utilized by the typestates of requesters and responders, and
